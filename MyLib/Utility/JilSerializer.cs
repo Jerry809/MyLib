5b1d4d914b661fbf18c641a8cf990c8b65d00bd0
BESTCO\jerryshih809 <horizon809@gmail.com>
2018-04-12 11:36:19 +0800

    init

---
+++ MyLib/Utility/JilSerializer.cs
@@ -0,0 +1,37 @@
+﻿using Jil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib.Utility
+{
+    public class JilSerializer
+    {
+        private static readonly Options _jilOptions = new Options(includeInherited: true,
+           dateFormat: DateTimeFormat.ISO8601);
+
+        /// <summary>
+        /// Json序列化(包含繼承的父類別)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static string Serialize<T>(T data)
+        {
+            return JSON.Serialize(data, _jilOptions);
+        }
+
+        /// <summary>
+        /// Json反序列化(包含繼承的父類別)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static T Deserialize<T>(string data)
+        {
+            return JSON.Deserialize<T>(data, _jilOptions);
+        }
+    }
+}
